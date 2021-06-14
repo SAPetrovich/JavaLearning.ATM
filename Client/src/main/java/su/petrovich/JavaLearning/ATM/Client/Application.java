54dcf2935b49bc030abfb5c495a8a48ef09f8c89
Александр Сысоев <APSysoeva@sberbank.ru>
2021-06-14 17:29:50 +0300

    Добавлена заготовка для клиента

---
+++ Client/src/main/java/su/petrovich/JavaLearning/ATM/Client/Application.java
@@ -0,0 +1,23 @@
+package su.petrovich.JavaLearning.ATM.Client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+
+    @Bean
+     public static CommandLineRunner logBeans() {
+        return (args) -> log.debug("────────────");
+    }
+
+}
