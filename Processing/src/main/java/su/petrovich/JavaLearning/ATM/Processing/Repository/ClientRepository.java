8683eea682d295ff45c0abcedf96569ce134a1f4
Александр Сысоев <APSysoeva@sberbank.ru>
2021-06-14 14:44:12 +0300

    Вернулся к использованию CrudRepository

--- Processing/src/main/java/su/petrovich/JavaLearning/ATM/Processing/Repository/ClientRepository.java
+++ Processing/src/main/java/su/petrovich/JavaLearning/ATM/Processing/Repository/ClientRepository.java
@@ -1,9 +1,9 @@
 package su.petrovich.JavaLearning.ATM.Processing.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import su.petrovich.JavaLearning.ATM.Processing.Entity.Client;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends CrudRepository<Client, Long> {
 }
